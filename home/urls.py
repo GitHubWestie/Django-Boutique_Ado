2c71b18e5d9f725aa855a690b4147d093c1eabd7
Keiran West <127797224+GitHubWestie@users.noreply.github.com>
2024-08-13 14:09:14 +0000

    added home app and templates

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+# The . tells python to get views from the same directory this urls.py is in
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home')
+]
