96b0863dd6521029f9ed4da7791970747f0e222c
Keiran West <127797224+GitHubWestie@users.noreply.github.com>
2024-09-21 19:28:32 +0000

    Getting ready to deploy

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 if os.path.exists('env.py'):
     import env
 
@@ -29,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 
-ALLOWED_HOSTS = [os.environ.get('LOCAL_HOST')]
+ALLOWED_HOSTS = [os.environ.get('LOCAL_HOST'), os.environ.get('HEROKU_HOST')]
 
 
 # Application definition
@@ -111,7 +112,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email' 
 ACCOUNT_EMAIL_REQUIRED = True 
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory' 
+ACCOUNT_EMAIL_VERIFICATION = 'none' 
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True 
 ACCOUNT_USERNAME_MIN_LENGTH = 4 
 LOGIN_URL = '/accounts/login/' 
@@ -123,12 +124,21 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        } 
+    }
+
+# DATABASES = {
+#     'default': dj_database_url.parse('postgres://uykpbglhlg8:OSoADem0MF92@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/grant_brink_ninth_367590')
+# }
 
 
 # Password validation
